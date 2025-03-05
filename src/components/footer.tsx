d246d949d7836833f662f4751b31faad566a71c7
João Manoel <joaomanoeldev@gmail.com>
2025-03-05 11:28:12 -0300

    styles: adjuste dark mode styles footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,12 +4,18 @@ import { BriefcaseBusiness, Github, Instagram, Mail } from "lucide-react"
 
 export const Footer = () => {
   return (
-    <footer className="bg-foreground flex justify-center items-center py-5">
+    <footer className="bg-foreground flex justify-center items-center py-5 dark:bg-zinc-900">
       <div className="max-w-6xl w-full flex justify-between items-center py-5">
         <div className="flex flex-col gap-2">
           <Link href="/" className="flex items-center gap-2">
-            <BriefcaseBusiness className="text-primary-foreground" />
-            <span className="font-semibold font-mono text-primary-foreground">Vagas.com</span>
+            <BriefcaseBusiness
+              className="text-primary-foreground dark:text-muted-foreground"
+            />
+            <span
+              className="font-semibold font-mono text-primary-foreground dark:text-muted-foreground"
+            >
+              Vagas.com
+            </span>
           </Link>
           <p
             className="text-muted-foreground w-96"
@@ -19,9 +25,9 @@ export const Footer = () => {
         </div>
 
         <div className="flex items-center gap-3">
-          <Instagram className="text-primary-foreground cursor-pointer" />
-          <Github className="text-primary-foreground cursor-pointer" />
-          <Mail className="text-primary-foreground cursor-pointer" />
+          <Instagram className="text-primary-foreground cursor-pointer dark:text-muted-foreground" />
+          <Github className="text-primary-foreground cursor-pointer dark:text-muted-foreground" />
+          <Mail className="text-primary-foreground cursor-pointer dark:text-muted-foreground" />
         </div>
       </div>
     </footer>
