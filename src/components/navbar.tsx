bd74714e2393114d9ec18cbefd5828efd91cf382
João Manoel <joaomanoeldev@gmail.com>
2025-03-05 11:27:46 -0300

    feat: add mode toggle button to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,6 +6,7 @@ import { BriefcaseBusiness } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
+import { ModeToggle } from "./mode-toggle"
 
 interface NavItems {
   label: string
@@ -35,29 +36,33 @@ export const Navbar = () => {
   const pathname = usePathname()
 
   return (
-    <nav className="flex justify-center border-b fixed top-0 right-0 left-0 z-50 bg-background">
+    <nav className="flex justify-center border-b fixed top-0 right-0 left-0 z-50 bg-background w-screen">
       <div className="max-w-6xl w-full flex items-center justify-between py-5">
         <Link href="/" className="flex items-center gap-2">
           <BriefcaseBusiness />
           <span className="font-semibold font-mono">Vagas.com</span>
         </Link>
 
-        <ul className="flex items-center gap-4">
-          {navItems.map(navItem => (
-            <Link
-              className={
-                cn(
-                  buttonVariants({ variant: "link" }),
-                  pathname === navItem.href && 'text-blue-500'
-                )
-              }
-              key={navItem.label}
-              href={navItem.href}
-            >
-              {navItem.label}
-            </Link>
-          ))}
-        </ul>
+        <div className="flex items-center gap-4">
+          <ul className="flex items-center gap-4">
+            {navItems.map(navItem => (
+              <Link
+                className={
+                  cn(
+                    buttonVariants({ variant: "link" }),
+                    pathname === navItem.href && 'text-blue-500'
+                  )
+                }
+                key={navItem.label}
+                href={navItem.href}
+              >
+                {navItem.label}
+              </Link>
+            ))}
+          </ul>
+
+          <ModeToggle />
+        </div>
       </div>
     </nav>
   )
