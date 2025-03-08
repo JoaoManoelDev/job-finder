bf3b36c90cea59edda05510310ff6051a29aa135
João Manoel <joaomanoeldev@gmail.com>
2025-03-08 18:31:16 -0300

    feat: create custom input

---
+++ src/components/custom-input.tsx
@@ -0,0 +1,17 @@
+import { ComponentProps } from "react"
+import { Label } from "@radix-ui/react-dropdown-menu"
+
+import { Input } from "@/components/ui/input"
+
+interface CustomInputProps extends ComponentProps<'input'> {
+  label?: string
+}
+
+export const CustomInput = ({ label, ...props }: CustomInputProps) => {
+  return (
+    <div className="flex flex-col gap-0.5">
+      <Label>{label}</Label>
+      <Input {...props} />
+    </div>
+  )
+}
