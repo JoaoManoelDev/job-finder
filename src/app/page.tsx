f8327d6f156e1ae2a5a9b789f9587c877e1b4c8b
João Manoel <joaomanoeldev@gmail.com>
2025-03-07 21:56:24 -0300

    feat: create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
+import Image from "next/image"
+
 import { Button } from "@/components/ui/button"
 
 const Home = () => {
   return (
-    <div>
-      <h1>Homepage</h1>
-      <Button>Click Me!</Button>
-    </div>
+    <section className="flex flex-col justify-center items-center py-10">
+      <h1 className="text-5xl font-black w-xl text-center">Diga adeus às longas buscas para uma vaga</h1>
+
+      <Button variant="outline" className="mt-10 h-11 w-60 border-foreground cursor-pointer text-lg font-mono">
+        Busque uma vaga
+      </Button>
+
+      <Image
+        className="h-[30rem] w-[30rem]"
+        alt=""
+        src="/people.svg"
+        width={100}
+        height={100}
+      />
+      
+    </section>
   )
 }
 
