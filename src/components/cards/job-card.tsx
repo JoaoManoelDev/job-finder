ae578bf62defd2173c817cee50847def5005b9cb
João Manoel <joaomanoeldev@gmail.com>
2025-03-08 13:14:01 -0300

    feat: create card job

---
+++ src/components/cards/job-card.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@/components/ui/button"
+
+export interface Job {
+  id: string
+  title: string
+  company: string
+  city: string
+  salary: string
+}
+
+interface JobCardProps {
+  job: Job
+}
+
+export const JobCard = ({ job }: JobCardProps) => {
+  return (
+    <article className="grid grid-cols-12 gap-4 items-center w-full border border-muted-foreground rounded-md p-6 font-semibold text-lg">
+      <h2 className="col-span-4">{job.title}</h2>
+      <p className="col-span-2">{job.company}</p>
+      <p className="col-span-2">{job.city}</p>
+      <p className="col-span-2">{job.salary}</p>
+      <Button variant="outline" className="cursor-pointer col-span-2">
+        Ver detalhes
+      </Button>
+    </article>
+  )
+}
