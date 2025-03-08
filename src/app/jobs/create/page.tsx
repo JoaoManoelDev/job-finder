557347df3f8762cdb6d3d8fc32ce78365422be94
João Manoel <joaomanoeldev@gmail.com>
2025-03-08 18:31:38 -0300

    feat: create form to create a new job

---
+++ src/app/jobs/create/page.tsx
@@ -0,0 +1,36 @@
+import { CustomInput } from "@/components/custom-input"
+import { CustomTextarea } from "@/components/custom-textarea"
+import { Button } from "@/components/ui/button"
+
+const CreateJobPage = () => {
+  return (
+    <main className="flex flex-col justify-center items-center py-8">
+      <div className="max-w-6xl w-full flex flex-col gap-4">
+        <h1 className="text-4xl font-semibold">Cadastrar vaga</h1>
+
+        <form className="flex flex-col gap-8">
+          <div className="flex gap-10">
+            <div className="flex flex-col flex-1 gap-5">
+              <CustomInput className="h-11" label="Título da Vaga" />
+              <CustomInput className="h-11" label="Nome da Empresa" />
+              <CustomInput className="h-11" label="Website da Empresa" />
+              <CustomInput className="h-11" label="Cidade" />
+              <CustomInput className="h-11" label="Período" />
+            </div>
+
+            <div className="flex flex-col flex-1 gap-5">
+              <CustomInput className="h-11" label="Período" />
+              <CustomInput className="h-11" label="Salário Mensal" />
+              <CustomTextarea label="Descrição da Vaga" className="resize-none h-[6.2rem]"/>
+              <CustomTextarea label="Requisitos" className="resize-none h-[6.2rem]" />
+            </div>
+
+          </div>
+          <Button className="w-36 ml-auto">Salvar</Button>
+        </form>
+      </div>
+    </main>
+  )
+}
+
+export default CreateJobPage
