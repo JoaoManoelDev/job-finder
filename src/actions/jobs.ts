7b02bba85ad8d1c23a3ddc3063157c60cb239b95
João Manoel <joaomanoeldev@gmail.com>
2025-03-18 21:31:34 -0300

    feat: create job actions

---
+++ src/actions/jobs.ts
@@ -0,0 +1,34 @@
+"use server"
+
+import { redirect } from "next/navigation"
+
+export const deleteJob = async (formData: FormData) => {
+  const jobId = formData.get('id')
+
+  const response = await fetch(`https://apis.codante.io/api/job-board/jobs/${jobId}`, {
+    method: 'DELETE',
+  })
+
+  if (response.ok) {
+    redirect('/jobs')
+  }
+}
+
+export const createJob = async (
+  _: unknown,
+  formData: FormData
+) => {
+  const response = await fetch(`https://apis.codante.io/api/job-board/jobs`, {
+    method: 'POST',
+    body: formData
+  })
+
+  if (!response.ok) {
+    return {
+      error: true,
+      message: (await response.json()).message
+    }
+  }
+  
+  redirect('/jobs')
+}
