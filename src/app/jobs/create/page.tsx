27b6fdfe95c204dfd03d974ec4b55f8fe18c8010
João Manoel <joaomanoeldev@gmail.com>
2025-03-18 21:36:36 -0300

    feat: migrate job form to separate component

--- src/app/jobs/create/page.tsx
+++ src/app/jobs/create/page.tsx
@@ -1,6 +1,4 @@
-import { CustomInput } from "@/src/components/custom-input"
-import { CustomTextarea } from "@/src/components/custom-textarea"
-import { Button } from "@/src/components/ui/button"
+import { CreateJobForm } from "./create-job-form"
 
 const CreateJobPage = () => {
   return (
@@ -8,26 +6,7 @@ const CreateJobPage = () => {
       <div className="max-w-6xl w-full flex flex-col gap-4">
         <h1 className="text-4xl font-semibold font-maven-pro">Cadastrar vaga</h1>
 
-        <form className="flex flex-col gap-8">
-          <div className="flex gap-10">
-            <div className="flex flex-col flex-1 gap-5">
-              <CustomInput className="h-11" label="Título da Vaga" />
-              <CustomInput className="h-11" label="Nome da Empresa" />
-              <CustomInput className="h-11" label="Website da Empresa" />
-              <CustomInput className="h-11" label="Cidade" />
-              <CustomInput className="h-11" label="Período" />
-            </div>
-
-            <div className="flex flex-col flex-1 gap-5">
-              <CustomInput className="h-11" label="Período" />
-              <CustomInput className="h-11" label="Salário Mensal" />
-              <CustomTextarea label="Descrição da Vaga" className="resize-none h-[6.2rem]"/>
-              <CustomTextarea label="Requisitos" className="resize-none h-[6.2rem]" />
-            </div>
-
-          </div>
-          <Button className="w-36 ml-auto">Salvar</Button>
-        </form>
+        <CreateJobForm />
       </div>
     </main>
   )
