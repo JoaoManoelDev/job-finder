123753c022be52e075fc532d2c1c1c2d5c7d0de2
João Manoel <joaomanoeldev@gmail.com>
2025-03-18 21:35:41 -0300

    feat: add fetch all jobs

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,30 +1,22 @@
 import { type Job, JobCard } from "@/src/components/cards/job-card"
 
-const allJobs: Job[] = [
-  {
-    id: '01',
-    city: 'Rio de Janeiro',
-    company: 'Google',
-    salary: 'R$ 8.000,00',
-    title: 'Desenvolvedor Full Stack',
-  },
-  {
-    id: '02',
-    city: 'São Paulo',
-    company: 'Meta',
-    salary: 'R$ 3.500,00',
-    title: 'Desenvolvedor Node Jr.',
-  },
-]
+interface GetJobsResponse {
+  data: Job[]
+}
 
-const JobsPage = () => {
+const JobsPage = async () => {
+  const response = await fetch('https://apis.codante.io/api/job-board/jobs', {
+    cache: 'no-store'
+  })
+  const jobs: GetJobsResponse = await response.json()
+
   return (
     <section className="flex flex-col justify-center items-center py-8">
       <div className="max-w-6xl w-full flex flex-col gap-4">
         <h1 className="text-4xl font-semibold font-maven-pro">Todas as vagas</h1>
 
         <div className="flex flex-col gap-4">
-          {allJobs.map((job) => {
+          {jobs.data.map((job) => {
             return (
               <JobCard key={job.id} job={job} />
             )
