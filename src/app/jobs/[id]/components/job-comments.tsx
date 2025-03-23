96db264e8905539b66e4cff8d600e07030d1edcf
João Manoel <joaomanoeldev@gmail.com>
2025-03-23 18:33:38 -0300

    feat: create job comments

---
+++ src/app/jobs/[id]/components/job-comments.tsx
@@ -0,0 +1,23 @@
+import { getJobComments } from "@/src/services/jobs/get-job-comments"
+
+interface JobCommentsProps {
+  jobId: string
+}
+
+export const JobComments = async ({ jobId }: JobCommentsProps) => {
+  const comments = await getJobComments({ jobId })
+
+  return (
+    <div className="flex flex-col gap-5">
+      {comments.data?.map((comment) => (
+        <div
+          key={comment.id}
+          className="flex flex-col gap-1 shadow-md px-4 py-2 rounded-sm border-l-4 border-l-blue-500"
+        >
+          <p className="font-semibold text-sm">{comment.author}</p>
+          <p className="leading-relaxed">{comment.content}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
