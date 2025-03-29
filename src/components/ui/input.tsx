89723b1bb7fab4d1bbe17caef8cf0d36541f30e5
João Manoel <joaomanoeldev@gmail.com>
2025-03-29 17:14:50 -0300

    feat: add icon property to input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,19 +2,28 @@ import * as React from "react"
 
 import { cn } from "@/src/lib/utils"
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+interface InputProps extends React.ComponentProps<"input"> {
+  icon?: React.ReactNode
+}
+
+function Input({ className, type, icon, ...props }: InputProps) {
   return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
-      )}
-      {...props}
-    />
+    <div className="relative">
+      {icon && <div className="absolute top-1/2 -translate-y-1/2 left-2.5">{icon}</div>}
+      
+      <input
+        type={type}
+        data-slot="input"
+        className={cn(
+          "border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-base file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-base",
+          "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+          "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+          className,
+          icon && "pl-9"
+        )}
+        {...props}
+      />
+    </div>
   )
 }
 
