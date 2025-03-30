f465d32f5bae70528c291c5c1ab7b05272d06164
João Manoel <joaomanoeldev@gmail.com>
2025-03-30 14:23:01 -0300

    refactor: use get jobs function

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,27 +1,45 @@
-import { type Job, JobCard } from "@/src/components/cards/job-card"
+import { JobCard } from "@/src/components/cards/job-card"
+import { SearchJobInput } from "@/src/components/jobs/search-job-input"
+import { getJobs } from "@/src/services/jobs/get-jobs"
+import { Search } from "lucide-react"
 
-interface GetJobsResponse {
-  data: Job[]
+interface JobsPageProps {
+  searchParams: Promise<{ [key: string]: string }>
 }
 
-const JobsPage = async () => {
-  const response = await fetch('https://apis.codante.io/api/job-board/jobs', {
-    cache: 'no-store'
-  })
-  const jobs: GetJobsResponse = await response.json()
+const JobsPage = async ({ searchParams }: JobsPageProps) => {
+  const params = await searchParams
+
+  const jobs = await getJobs({ search: params.search })
 
   return (
     <section className="flex flex-col justify-center items-center py-8">
       <div className="max-w-6xl w-full flex flex-col gap-4">
-        <h1 className="text-4xl font-semibold font-maven-pro">Todas as vagas</h1>
+        <h1 className="text-4xl font-semibold font-maven-pro">Vagas</h1>
 
-        <div className="flex flex-col gap-4">
-          {jobs.data.map((job) => {
-            return (
-              <JobCard key={job.id} job={job} />
-            )
-          })}
-        </div>
+        <SearchJobInput />
+
+        {jobs.data?.length === 0 && (
+          <div className="flex gap-2 items-center justify-center h-96">
+
+            <Search className="h-8 w-8" />
+            <p className="text-2xl">Nenhuma vaga encontrada...</p>
+          </div>
+        )}
+
+        {!jobs.error && jobs.data ? (
+          <div className="flex flex-col gap-4">
+            {jobs.data?.map((job) => {
+              return (
+                <JobCard key={job.id} job={job} />
+              )
+            })}
+          </div>
+        ) : (
+          <div className="flex flex-col gap-8 items-center justify-center h-96">
+            <h1 className="text-3xl font-bold">{jobs.messageError} 😥</h1>
+          </div>
+        )}
       </div>
     </section>
   )
