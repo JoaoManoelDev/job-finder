75df3a5f872ef8c946d10720ff8ad0051d9a4451
João Manoel <joaomanoeldev@gmail.com>
2025-04-06 13:52:23 -0300

    types: add JobsLinks and JobsMeta types

--- src/services/jobs/interfaces.ts
+++ src/services/jobs/interfaces.ts
@@ -13,6 +13,24 @@ export type Job = {
   updated_at: string
 }
 
+export type JobsLinks = {
+  first: string,
+  last: string
+  prev: string | null,
+  next: string | null
+}
+
+export type JobsMeta = {
+  current_page: number,
+  from: number,
+  last_page: number,
+  // links: []
+  path: string,
+  per_page: number,
+  to: number,
+  total: number
+}
+
 export type Comment = {
   id: string
   author: string
